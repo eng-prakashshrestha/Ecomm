e518e2fa562ce3d11dd5f0a3d8091ca8b6b05274
eng-prakashshrestha <engshresthaprakash@gmail.com>
2022-10-19 11:08:58 +1100

    Admin Database created

---
+++ ecom-app/database/migrations/2022_10_19_000001_create_admins_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->String('name');
+            $table->String('email')->unique();
+            $table->String('password');
+            $table->String('image');
+            $table->String('mobile');
+            $table->integer('row_id')->unsigned();
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+};
